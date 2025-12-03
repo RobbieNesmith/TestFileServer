92ae9873c88ca080af404cee43a422db22c7cbd2
Robbie Nesmith <robbie@newbo.co>
2025-12-03 13:48:06 -0600

    Allow specifying file to serve

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
+import argparse
+
 from flask import Flask, send_file
+
+parser = argparse.ArgumentParser("TestFileServer", description="A server that serves the specified file at every route.")
+
+parser.add_argument("filename")
+parser.add_argument("--host", default="localhost")
+parser.add_argument("--port", default="5000")
+
+args = parser.parse_args()
 
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-  resp = send_file("Trollface.png")
+  resp = send_file(args.filename)
   resp.access_control_allow_origin = "*"
   return resp
 if __name__ == '__main__':
-  app.run()
+  app.run(args.host, args.port)
